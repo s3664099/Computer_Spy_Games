96d4a0b693c4d47045bba228632b5621ff7ea0a1
David Sarkies <david.sarkies@internode.on.net>
2024-03-22 13:38:41 +1100

    Created set up for Spy Eyes

--- spy_eyes.py
+++ spy_eyes.py
@@ -22,8 +22,44 @@ instructions = "{}a hawk while you press a key (any one will do). One of them mo
 instructions = "{}When you think you know, press a key again and tell the computer.\n".format(instructions)
 instructions = "{}  Bet your powers of observation aren't as good as you thought.\n".format(instructions)
 
+number_size = 9
+numbers = []
+used_coords = []
+screen_size = 9
+
 def main_game():
+
+	score = 0
 	
+	for x in range(number_size):
+
+		numbers.append(generate_tupple(x+1))
+
+#Selects random numbers and confirms they have not been used
+def generate_tupple(num):
+
+	available_coord = False
+
+	while (available_coord == False):
+
+		found_coord = False
+		x_coord = randint(4,screen_size+4)
+		y_coord = randint(4,screen_size+4)
+
+		for x in used_coords:
+
+			if x_coord == x[0] and y_coord == x[1]:
+				found_coord == True
+
+
+
+		if found_coord == False:
+			available_coord = True
+			used_coords.append((x_coord,y_coord))
+
+	return ([x_coord,y_coord],num)
+
+
 	#Set up an array holding nine tuples which holds the x,y positions and the number
 	#Randomly assigns the positions (though they cannot be the same, or within 2 points)
 	#Randomly selects one and moves it
@@ -32,6 +68,49 @@ def main_game():
 	#After three shots game over
 	#Saves a high score
 	#Makes High Score persistant
+
+	"""
+	*10 DIM X(9): DIM Y(9)
+	*20 LET P=0
+	*30 FOR I=1 TO 9
+	*40 GOSUB 340: LET X(I)=N+3
+	*50 GOSUB 340: LET Y(I)=N+3
+	60 NEXT I
+	70 GOSUB 360
+	80 GOSUB 310
+	90 GOSUB 340
+	100 LET M=N:GOSUB 340
+	110 LET X(M)=X(M)+SGN(N-5.1)
+	120 GOSUB 360
+	130 GOSUB 310
+	140 CLS:PRINT
+	150 PRINT "WHICH NUMBER MOVED"
+	160 INPUT A
+	170 IF A<>M THEN GOTO 250
+	180 CLS:PRINT
+	190 PRINT "WELL SPIED!"
+	200 LET P=P+!
+	210 PRINT "YOU NOW HAVE ";P;" POINTS"
+	220 PRINT: PRINT "PRESS A KEY"
+	230 GOSUB 310
+	240 GOTO 30
+	250 CLS:PRINT:PRINT "WRONG - END OF GO"
+	260 PRINT "CORRECT ANSWER WAS ";M
+	270 PRINT "YOU SCORED ";P;" POINTS"
+	280 PRINT "ANOTHER GO? (Y/N)"
+	290 INPUT A$:IF A$="Y" THEN RUN
+	300 STOP
+	310 LET I$=INKEY$
+	320 IF I$="" THEN GOTO 310
+	330 RETURN
+	*340 LET N=INT(RND(1)*9)+1
+	350 RETURN
+	360 CLS
+	370 FOR I=1 TO 9
+	380 PRINT TAB(X(I),Y(I));STR$(I)
+	390 NEXT I
+	400 RETURN
+	"""
 
 #Passes the current file as a module to the loader
 if __name__ == '__main__':
