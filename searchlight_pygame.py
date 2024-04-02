05261d16f1b0dd8e4b1422c89c855feb8b415acb
David Sarkies <david.sarkies@internode.on.net>
2024-04-02 21:38:36 +1100

    Added pygame module for searchlight

---
+++ searchlight_pygame.py
@@ -0,0 +1,18 @@
+"""
+Title: Graphics Package for Searchlight
+Translator: David Sarkies
+Version: 1
+Date: 2 April 2023
+
+This file holds a series of functions that enable graphical displays using pygame.
+
+To install pygame you need to do the following:
+
+pip3 install pygame
+"""
+
+import pygame
+
+cell_size = 50
+columns = 20
+rows = 15
